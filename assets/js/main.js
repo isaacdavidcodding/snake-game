84d7ab3b62686621e2ab37c5e7283b473caf3e70
Isaac David <isaacdavid797@gmail.com>
2021-05-07 19:33:26 -0300

    snake game developed in front bootcamp

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+let canvas = document.getElementById('snake');
+let context = canvas.getContext('2d');
+let box = 32;
+
+let snake = [];
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
+
+let direction = 'right';
+
+let square = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+function createBG() {
+  context.fillStyle = 'darkmagenta';
+  context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = 'black';
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
+function showSquare() {
+  context.fillStyle = 'yellow'
+  context.fillRect(square.x, square.y, box, box);
+}
+
+function update(event) {
+  if (event.keyCode == 37 && direction != 'right') direction = 'left';
+  if (event.keyCode == 38 && direction != 'down') direction = 'up';
+  if (event.keyCode == 39 && direction != 'left') direction = 'right';
+  if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
+document.addEventListener('keydown', update);
+
+function startGame() {
+
+  if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+  if (snake[0].x < 0  && direction == 'left') snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+  if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+  for (let k = 1; k < snake.length; k++) {
+    if (snake[0].x == snake[k].x && snake[0].y == snake[k].y) {
+      clearInterval(game);
+      alert('Its Over!');
+    }
+  }
+    
+  createBG();
+  createSnake();
+  showSquare();
+
+  let snakeX = snake[0].x; 
+  let snakeY = snake[0].y;
+
+  if (direction == 'right') snakeX += box;
+  if (direction == 'left') snakeX -= box;
+  if (direction == 'up') snakeY -= box;
+  if (direction == 'down') snakeY += box;
+
+  if (snakeX != square.x || snakeY != square.y) snake.pop();
+  else {
+    square.x = Math.floor(Math.random() * 15 + 1) * box;
+    square.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
+
+  let newHead = { x: snakeX, y: snakeY }
+
+  snake.unshift(newHead);
+}
+
+let game = setInterval(startGame, 100);
